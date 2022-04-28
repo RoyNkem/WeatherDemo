2e6c4c867e84d5c6c76a9c08a05af445e0766c3e
Aiyetin Roy <aiyetinroy@gmail.com>
2022-04-28 16:56:22 +0100

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Roy Aiyetin on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
